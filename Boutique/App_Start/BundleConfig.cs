71b0ca870138b940a017684c6053f0b1a8f23e14
ahmedmohnada@gmail.com <ahmedmohnada@gmail.com>
2020-10-19 10:51:29 +0200

    add Layout page and css files and js files

--- Boutique/App_Start/BundleConfig.cs
+++ Boutique/App_Start/BundleConfig.cs
@@ -20,11 +20,14 @@ namespace Boutique
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/umd/popper.min.js",
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
